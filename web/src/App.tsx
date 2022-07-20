cc466521b72a0cc53e61ab3b7051f504d19b2f47
Alex Acebo <alex.acebo@kustomer.com>
2022-07-20 17:05:14 -0400

    stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Settings from './pages/Settings';
 
 export default function App() {
+  useEffect(() => {
+    window.Kustomer.initialize({ }, (contextJSON: any) => {
+      if (contextJSON) {
+        window.Kustomer.resize();
+      }
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
