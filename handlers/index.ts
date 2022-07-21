2b848f6e5b6daa9cb7c8d213f791f08444242b84
Alex Acebo <alex.acebo@kustomer.com>
2022-07-21 18:06:53 -0400

    stuff

--- handlers/index.ts
+++ handlers/index.ts
@@ -2,5 +2,3 @@ export * from './on-conversation-update';
 export * from './on-get-settings';
 export * from './on-set-settings';
 export * from './on-get-team-channels';
-export * from './on-auth';
-export * from './on-auth-complete';
